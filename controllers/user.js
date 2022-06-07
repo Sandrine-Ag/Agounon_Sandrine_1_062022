5f96a6037bbc3ac3607da86b4389aede2cfe8a6f
Tchacorom Sandrine <doudouvi.sand@gmail.com>
2022-06-07 15:25:24 +0000

    Déclaration de fonction signup et login

---
+++ controllers/user.js
@@ -0,0 +1,10 @@
+
+// La fonction signup pour création de nouveaux users dans la base de données
+exports.signup = (req, res, next) => {
+    
+};
+
+// La fonction login pour connecter des users existants
+exports.login = (req, res, next) => {
+    
+};
