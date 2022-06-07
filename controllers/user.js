055d4505e983fb135f50d6b0131ec4e3bd533744
Tchacorom Sandrine <doudouvi.sand@gmail.com>
2022-06-07 18:59:30 +0000

    Implémentation de la fonction async signup d'un nouvel utilisateur avec password hashé

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,30 @@
+// On a besoin de notre modèle User pour enrégistrer et lire des users dans ces middlewares
+const User = require('../models/User');
+
+// on a besoin du package de cryptage 
+//Importation du package de cryptage pour les mots de passe
+const bcrypt = require('bcrypt');
 
 // La fonction signup pour création de nouveaux users dans la base de données
 exports.signup = (req, res, next) => {
-    
+    // on crypte en lui passant le mot de passe du corps de la requête aui sera passé par le frontend
+    // combien de fois on exécute l'algorithme de hachage(10 tours)
+     bcrypt.hash(req.body.password, 10)
+    //  on récupère le hash de notre mot de passe
+     .then(hash => { 
+       //on enrégistre dans un nouveau user dans la base de données avec notre modèle mongoose
+        const user = new User({
+            //on passe l'adresse dans le corps de la requête
+            email: req.body.email,
+            // on enrégistre le mot de passe haché
+            password: hash
+       });
+       //Enregistrement du user dans la base de données
+       user.save()
+           .then(() => res.status(201).json({message: "Utilisateur créé !"}))
+           .catch(error => res.status(400).json({error}));
+   })
+   .catch(error => res.status(500).json({error}));
 };
 
 // La fonction login pour connecter des users existants
