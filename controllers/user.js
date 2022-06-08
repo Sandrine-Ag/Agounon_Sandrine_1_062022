51b228915cb7b6cc362e206c99ca71b37ba23d9f
Tchacorom Sandrine <doudouvi.sand@gmail.com>
2022-06-08 22:11:21 +0000

    Route d'authentification login

--- controllers/user.js
+++ controllers/user.js
@@ -29,5 +29,39 @@ exports.signup = (req, res, next) => {
 
 // La fonction login pour connecter des users existants
 exports.login = (req, res, next) => {
+    // on trouve un user dans la base de données qui correspond 
+    //à l'adresse mail qui est entrée par le user de l'application
+    // adresse mail correspond à l'adresse mail envoyée dans la requête
+    User.findOne({email: req.body.email})
+        .then(user => {
+            // si on ne trouve pas de User
+            if (!user) {
+                return res.status(401).json({error: "Utilisateur non trouvé !"});   
+            }
+            //la fonction compare pour comparer le mot de passe envoyé 
+            // avec la requête avec le hash qui est enregistré dans le document user
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    // si le mot de passe ne correspond pas
+                    if (!valid) {
+                        return res.status(401).json({error: "Mot de passe incorrect !"})
+                    }
+                    // si oui
+                    //identifiant de user dans la base renvoie son user id et son token d'identification
+                    res.status(200).json({
+                        userId: user._id,
+                        token
+                    });
+                })
+                // problème de connexion lié à MongoDB
+                .catch(error => res.status(500).json({error}));
+        })
+        .catch(error => res.status(500).json({error}));
+    
+
+    
+
+
+
     
 };
